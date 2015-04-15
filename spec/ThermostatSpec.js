3348af66ff4527ada43b0864d3fd09a824e05a81
sanjsanj <sanjsanj@hotmail.com>
2015-04-15 15:57:23 +0100

    Thermostat can decrease temp, has minimum temp of 10, has power saving mode on by default

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -11,6 +11,10 @@ describe('Thermostat', function() {
     it('starts at 20 degrees', function() {
       expect(thermostat.temp()).toBe(20);
     });
+
+    it('has power saving mode on', function() {
+      expect(thermostat.psStatus()).toBe(true);
+    });
   });
 
   describe('can', function() {
@@ -18,6 +22,29 @@ describe('Thermostat', function() {
     it('increase temp by 1 with the \'up\' button', function() {
       thermostat.upButton();
       expect(thermostat.temp()).toBe(21);
+    });
+
+    it('decrease temp by 1 with the \'down\' button', function() {
+      thermostat.downButton();
+      expect(thermostat.temp()).toBe(19);
+    });
+    
+  });
+
+  describe('has', function() {
+
+    it('a minimum temp of 10', function() {
+      for (var i = 0; i < 20; i ++) {
+        thermostat.downButton();
+      };
+      expect(thermostat.temp()).toBe(10);
+    });
+
+    it('a maximum temp of 25 with power saving mode on', function() {
+      for (var i = 0; i < 20; i ++) {
+        thermostat.upButton();
+      };
+      expect(thermostat.temp()).toBe(25);
     });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,5 +1,6 @@
 var Thermostat = function() {
   this._temp = 20;
+  this._psMode = true;
 };
 
 Thermostat.prototype.temp = function() {
@@ -7,5 +8,17 @@ Thermostat.prototype.temp = function() {
 };
 
 Thermostat.prototype.upButton = function() {
-  this._temp += 1;
+  if (this._temp < 25) {
+    this._temp ++;
+  };
+};
+
+Thermostat.prototype.downButton = function() {
+  if (this._temp > 10) {
+    this._temp --;
+  };
+};
+
+Thermostat.prototype.psStatus = function() {
+  return this._psMode;
 };
