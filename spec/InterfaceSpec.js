064efe496dcf0ffda997ecc43c233b7cd9aafcd4
stacca <caronstace@gmail.com>
2015-04-16 13:33:22 +0100

    adding increase button

--- spec/InterfaceSpec.js
+++ spec/InterfaceSpec.js
@@ -10,7 +10,12 @@ describe('Interface', function() {
   });
 
   it('can increase temp by 1', function(){
-    $("input#up").click();
+    $("#up").click();
     expect('#temp').toContainText('21');
+  });
+  
+  it('can decrease temp by 1', function(){
+    $('#down').click();
+    expect('#temp').toContainText('19');
   });
 });
--- src/Interface.js
+++ src/Interface.js
@@ -1,5 +1,11 @@
 var thermostat = new Thermostat();
 
 $(document).ready(function(){
+  
   $('#temp').text(thermostat.temp());
+  $('#up').click(function(){
+    thermostat.upButton();
+   $('#temp').text(thermostat.temp());
+  });
+ 
 });
