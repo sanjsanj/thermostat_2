50123e62060910838afe69dc928182ae2ec26245
sanjsanj <sanjsanj@hotmail.com>
2015-04-22 11:03:27 +0100

    Add API local temp call

--- src/Interface.js
+++ src/Interface.js
@@ -2,12 +2,25 @@ var thermostat = new Thermostat();
 
 $(document).ready(function(){
 
+  var OpenWeather = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk'
+
   $('#powerbutton').click(function(){
-    $('.container').toggle();
+    $('.container').fadeToggle();
     thermostat.resetButton();
     updateColor();
     $('#temp').text(thermostat.temp());
   });
+
+  $.getJSON(OpenWeather, function(data) {
+    getTemp = data.main.temp;
+    temp = temperatureConverter(getTemp);
+    $('#outsideTemperature').html(temp);
+  });
+
+  function temperatureConverter(temp){
+    temp = temp - 273.15;
+    return temp.toFixed(1);
+  };
 
   var updateColor = function() {
      if (thermostat.tempColor() === 'green' ) {
